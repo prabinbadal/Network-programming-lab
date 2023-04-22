35fef9344c01fc10c71972b6424bf4e9fcc2a31d
Prabin Badal <131003752+prabinbadal@users.noreply.github.com>
2023-04-22 21:48:16 +0545

    Add files via upload

---
+++ DataReader.java
@@ -0,0 +1,20 @@
+package DataReaderServer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class DataReader {
+
+    public static void main(String[] args) throws Exception {
+        URL url = new URL("https://www.example.com/data");
+        URLConnection connection = url.openConnection();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+        reader.close();
+    }
+}
+
---
+++ HeaderFieldsRetriever.java
@@ -0,0 +1,20 @@
+package HeaderFields;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
+
+public class HeaderFieldsRetriever {
+
+    public static void main(String[] args) throws Exception {
+        URL url = new URL("https://www.example.com");
+        URLConnection connection = url.openConnection();
+        Map<String, List<String>> headerFields = connection.getHeaderFields();
+        for (Map.Entry<String, List<String>> entry : headerFields.entrySet()) {
+            String key = entry.getKey();
+            List<String> values = entry.getValue();
+            System.out.println(key + ": " + values);
+        }
+    }
+}
+
