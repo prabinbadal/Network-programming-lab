b4f7066973956a0e12880bfcda083191a531a19c
Prabin Badal <131003752+prabinbadal@users.noreply.github.com>
2023-04-23 20:30:17 +0545

    Update DataReader.java

--- DataReader.java
+++ DataReader.java
@@ -7,7 +7,7 @@ import java.net.URLConnection;
 public class DataReader {
 
     public static void main(String[] args) throws Exception {
-        URL url = new URL("https://www.example.com/data");
+        URL url = new URL("https://www.viber.com/data");
         URLConnection connection = url.openConnection();
         BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         String line;
